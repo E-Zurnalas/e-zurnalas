2299458f4c6ca3a693c47e79b5126244bea8cf68
Ignas <ignas.sadaunykas@gmail.com>
2022-09-29 22:13:14 +0300

    Update scripts.php

--- e-zurnalas/inc/scripts.php
+++ e-zurnalas/inc/scripts.php
@@ -6,10 +6,10 @@ function e_zurnalas_scripts() {
 	wp_enqueue_style( 'e-zurnalas-style', get_stylesheet_uri(), array(), _S_VERSION );
 	
 	wp_enqueue_script( 'e-zurnalas-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
-	wp_enqueue_script('jquery');
+	//wp_enqueue_script('jquery');
 	
 	if(is_page_template('templates/login-template.php')) {
-		wp_enqueue_script( 'login', get_template_directory_uri() . '/assets/js/login.js', array(), _S_VERSION, true);
+		wp_enqueue_script( 'login', get_template_directory_uri() . '/assets/js/login.js', array('jquery'), _S_VERSION, true);
 	}
 	
 }
