cec53dd64184c87c588ae1be6aa14fba0b3f80f0
Ignas <ignas.sadaunykas@gmail.com>
2022-09-29 21:46:08 +0300

    test

--- e-zurnalas/templates/login-template.php
+++ e-zurnalas/templates/login-template.php
@@ -13,8 +13,6 @@ get_header(); ?>
                 <?php _e('Sveikas sugrįžęs! Prašome prisijungti prie savo paskyros.' , 'e-zurnalas'); ?>
             </p>
             <form id="login-form">
-                <p class="form-row">
-            <form method="post" id="login-form">
                 <p class="form-row input-login-wrap">
                     <input id="username" name="username" type="text" autocomplete="username" placeholder="El. Paštas">
                 </p>
