ff7aa226b0bd21a2002abb2899dd04b78b00b837
Ignas <ignas.sadaunykas@gmail.com>
2022-09-29 20:22:16 +0300

    Update login-template.php

--- e-zurnalas/templates/login-template.php
+++ e-zurnalas/templates/login-template.php
@@ -5,5 +5,17 @@ get_header(); ?>
 
 <h1>Yeee login</h1>
 
+<div class="login-section">
+    <div class="first-box">
+    </div>
+    <div class="second-box">
+        <div class="login-container">
+            <img class="login-logo" src="" width="" height="" alt="">
+            <p class="welcome-text">
+                </php _e('Sveikas sugrįžęs! Prašome prisijungti prie savo paskyros.' , 'e-zurnalas'); ?>
+            </p>
+        </div>
+    </div>
+</div>
 
 <?php get_footer();
