d1890ec0701fc92751f7f8aa5d109cf009bc1502
Ričardas <ricardas.remis@gmail.com>
2022-09-29 21:37:53 +0300

    b

--- e-zurnalas/templates/login-template.php
+++ e-zurnalas/templates/login-template.php
@@ -13,7 +13,7 @@ get_header(); ?>
                 <?php _e('Sveikas sugrįžęs! Prašome prisijungti prie savo paskyros.' , 'e-zurnalas'); ?>
             </p>
             <form method="post" action="" id="login-form">
-                <p class="form-row">
+                <p class="form-row input-login-wrap">
                     <input id="username" name="username" type="text" autocomplete="username" placeholder="El. Paštas">
                 </p>
                 <p class="form-row">
