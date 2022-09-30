626fbc86782611374bc56bc0c1d3bf5f8ea69f85
Ričardas <ricardas.remis@gmail.com>
2022-09-30 22:31:01 +0300

    dashboard

--- e-zurnalas/templates/dashboard-template.php
+++ e-zurnalas/templates/dashboard-template.php
@@ -5,20 +5,22 @@ get_header(); ?>
 
 <div id="admin-page-full-wrap">
     <div id="admin-menu-wrap">
-        <div id="admin-page-back"></div>
-        <div id="admin-page-top-bar" class=""></div>
-        <div id="admin-page-menu">
-
-        <?php
-        wp_nav_menu(
-            array(
-            'theme_location' => 'admin_page_menu',
-            'menu_id'        => 'admin_panel_menu',
-            )
-        );
-        ?>
+         <div id="admin-page-back"></div>
+            <div id="admin-page-top-bar" class=""></div>
+            <div id="admin-page-menu">
+                <div class="main-logo">
+                    <img class="login-logo" src="<?php echo get_template_directory_uri() . "/assets/images/logo.png"; ?>" width="167" height="41" alt="">
+                </div>
+                <?php
+                    wp_nav_menu(
+                    array(
+                    'theme_location' => 'admin_page_menu',
+                    'menu_id'        => 'admin_panel_menu',
+                    )
+                    );
+                ?>
         </div>
-    </div>
+</div>
 
 
 
