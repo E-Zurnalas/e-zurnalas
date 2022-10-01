7119d0a6c6ede25164dc59d444f10b6282e7f00f
Ričardas <ricardas.remis@gmail.com>
2022-10-01 16:22:23 +0300

    d

--- e-zurnalas/templates/login-template.php
+++ e-zurnalas/templates/login-template.php
@@ -26,6 +26,7 @@ get_header(); ?>
 
     <div class="first-box">
         <img class="login-logo" src="<?php echo get_template_directory_uri() . "/assets/images/logo-white.svg"; ?>" width="240" height="60" alt="">
+        <img class="login-logo" src="<?php echo get_template_directory_uri() . "/assets/images/auth.png"; ?>" alt="">
     </div>
 </div>
 
