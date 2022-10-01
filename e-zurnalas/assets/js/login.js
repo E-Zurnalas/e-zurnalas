af1bf62d261ac04265623b369e65c526e7cda957
Ignas <ignas.sadaunykas@gmail.com>
2022-10-01 15:56:19 +0300

    Update login.js

--- e-zurnalas/assets/js/login.js
+++ e-zurnalas/assets/js/login.js
@@ -3,18 +3,29 @@ jQuery(document).ready(function(){
 		event.preventDefault();
 		let login = jQuery('#username').val();
 		let pass = jQuery('#password').val();
-		jQuery.ajax({
-			url: loginajax.ajaxUrl,
-			data : {
-				user: login,
-				pass: pass,
-				action : 'login',
-			},
-			type: 'POST',
-			success: function (data) {
-				console.log(data);
-			}
-		});
+		let valid = 0;
+		if(isEmail(login))
+		{
+			valid = 1;
+		}
+		if(valid) {
+			jQuery.ajax({
+				url: loginajax.ajaxUrl,
+				data: {
+					user: login,
+					pass: pass,
+					action: 'login',
+				},
+				type: 'POST',
+				success: function (data) {
+					console.log(data);
+				}
+			});
+		}
 	});
 });
 
+function isEmail(email) {
+	let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email);
+}
