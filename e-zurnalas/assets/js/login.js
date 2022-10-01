101f27dd6d639f885b13e1126f9dcbd7a89de126
Ignas <ignas.sadaunykas@gmail.com>
2022-10-01 15:46:26 +0300

    Update login.js

--- e-zurnalas/assets/js/login.js
+++ e-zurnalas/assets/js/login.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
 	jQuery('#login-form').on("submit", function(event){
 		event.preventDefault();
 		let login = jQuery('#username').val();
-		let pass = jQuery('$password').val();
+		let pass = jQuery('#password').val();
 		console.log(login + ' - ' + pass);
 	});
 });
