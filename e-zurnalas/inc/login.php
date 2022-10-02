1b1119868be98104d2fb854fbf78200b0b53178e
Ignas <ignas.sadaunykas@gmail.com>
2022-10-02 17:36:43 +0300

    Update login.php

--- e-zurnalas/inc/login.php
+++ e-zurnalas/inc/login.php
@@ -20,9 +20,20 @@ function login(): void {
 add_filter( 'wp_ajax_nopriv_login', 'login' );
 add_filter( 'wp_ajax_login', 'login' );
 
+// User logged in, so redirect out of login page to home page
 function user_logedin_redirect(): void {
 	if(is_user_logged_in() && is_page_template('templates/login-template.php')) :
 		$url = get_home_url();
+		header( 'Location: ' . $url);
+	endif;
+}
+
+add_action( 'template_redirect', 'user_needs_to_login' );
+
+// User not logged in, so redirect to login page
+function user_needs_to_login(): void {
+	if(!is_user_logged_in() && !is_page_template('templates/login-template.php')) :
+		$url = get_page_link(14);
 		header( 'Location: ' . $url);
 	endif;
 }
